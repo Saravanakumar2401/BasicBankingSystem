e9a017a0749d234d62b9127578eb5cd08dac05c2
Saravanakumar2401 <90196121+Saravanakumar2401@users.noreply.github.com>
2021-10-21 15:56:50 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+<head> 
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="./css/index.css">
+    <link rel="stylesheet" type="text/css" href="./css/navbar.css">
+    
+    <title>Basic Banking System</title>  
+</head>
+<style>
+    body{
+        background-color:#5de8b7;
+        background-repeat:repeat;
+        background-size:100%;
+    }
+    </style>
+<body>
+    <?php
+    include 'navbar.php';
+    ?>
+
+    <!-- Start About area -->
+    <section id="about" class="site-about">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-12  about-image">
+                  <br><br><br><br><img src="image/about.png" alt="banner-img" class="img-fluid">
+                    
+                </div>
+                <div class="col-lg-6 col-md-12 about">
+                    <h1><br><center>ABOUT US</center></h1>
+                  <h4 class="title-text">Our Bank consists of many activities that can be done through a number of financial institutions that accept deposits from individuals and other entities, and then use this money to offer loans, and to invest and earn profit.
+                    <br><br>This is my project under the The Sparks Foundation as a part of Internship.In which a dynamic website has to be created with dummy 10 customers and transaction to be made between them.<br>
+			        <br>Here,I have used HTML,CSS as a frontend and PHP as backend and MYSQL as a database. 
+                  <br>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- End About area -->
+
+    <!-- Start Footer area -->   
+    <footer class="text-center mt-5 py-2">
+            <p>&copy 2021. Made by <b>SARAVANA KUMAR R</b> <br> for the Project of <br> <b>The Sparks Foundation</b></p>
+    </footer>
+    <!-- End Footer area -->
+</body>
+</html>
